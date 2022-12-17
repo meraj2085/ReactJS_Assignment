daf0f894b83dcd1af691fa0d70a6e40f9939e62b
meraj2085 <samirmeraj60@gmail.com>
2022-12-17 17:10:08 +0600

    Adding tailwind, DaisyUI and Pages

---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+     return (
+          <div className='bg-slate-400 py-5 text-center'>
+               <h1 className='text-2xl font-semibold'>Test Application</h1>
+          </div>
+     );
+};
+
+export default Navbar;
