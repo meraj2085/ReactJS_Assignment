e6f7447080f38267378771d1cc7c4d44e47cd1b7
meraj2085 <samirmeraj60@gmail.com>
2022-12-18 10:28:38 +0600

    Adding data in local storage & getting time

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { GetTime } from "../Utilities/GetTime";
 import Buttons from "./Buttons";
 
 const Home = () => {
+  const [btn1Data, setBtn1Data] = useState(null);
+  const [btn2Data, setBtn2Data] = useState(null);
+  const [btn3Data, setBtn3Data] = useState(null);
+  const [btn4Data, setBtn4Data] = useState(null);
+
   const selectedBtn = (btn) => {
     const api = btn.api;
     GetTime(api)
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err.message));
+      .then((data) => {
+        if (btn.name === "Button 1") {
+          setBtn1Data(data);
+        } else if (btn.name === "Button 2") {
+          setBtn2Data(data);
+        } else if (btn.name === "Button 3") {
+          setBtn3Data(data);
+        } else {
+          setBtn4Data(data);
+        }
+      })
+      .catch((err) => console.log(err.message));
   };
 
   return (
@@ -15,34 +30,34 @@ const Home = () => {
       <div className="grid grid-cols-2 gap-10 mt-20 mb-10">
         <div className="card bg-base-200 shadow-xl">
           <div className="card-body text-lg">
-            <p>Start: </p>
-            <p>End: </p>
-            <p>Start Save: </p>
-            <p>End Save: </p>
+            <p>Start: {btn1Data?.start}</p>
+            <p>End: {btn1Data?.end}</p>
+            <p>Start Save: {btn1Data?.startSave}</p>
+            <p>End Save: {btn1Data?.endSave}</p>
           </div>
         </div>
         <div className="card bg-base-200 shadow-xl">
           <div className="card-body text-lg">
-            <p>Start: </p>
-            <p>End: </p>
-            <p>Start Save: </p>
-            <p>End Save: </p>
+            <p>Start: {btn2Data?.start}</p>
+            <p>End: {btn2Data?.end}</p>
+            <p>Start Save: {btn2Data?.startSave}</p>
+            <p>End Save: {btn2Data?.endSave}</p>
           </div>
         </div>
         <div className="card bg-base-200 shadow-xl">
           <div className="card-body text-lg">
-            <p>Start: </p>
-            <p>End: </p>
-            <p>Start Save: </p>
-            <p>End Save: </p>
+            <p>Start: {btn3Data?.start}</p>
+            <p>End: {btn3Data?.end}</p>
+            <p>Start Save: {btn3Data?.startSave}</p>
+            <p>End Save: {btn3Data?.endSave}</p>
           </div>
         </div>
         <div className="card bg-base-200 shadow-xl">
           <div className="card-body text-lg">
-            <p>Start: </p>
-            <p>End: </p>
-            <p>Start Save: </p>
-            <p>End Save: </p>
+            <p>Start: {btn4Data?.start}</p>
+            <p>End: {btn4Data?.end}</p>
+            <p>Start Save: {btn4Data?.startSave}</p>
+            <p>End Save: {btn4Data?.endSave}</p>
           </div>
         </div>
       </div>
--- src/Utilities/GetTime.js
+++ src/Utilities/GetTime.js
@@ -3,5 +3,19 @@ export const GetTime = async (api) => {
   const res = await fetch(api);
   const data = await res.json();
   const end = Date.now();
-  return {data, start, end};
+
+
+  const storedData = localStorage.getItem("Button_Data");
+  let ButtonData;
+  if (!storedData) {
+    ButtonData = [];
+  }else{
+    ButtonData = JSON.parse(storedData);
+  }
+  ButtonData.push(data)
+  const startSave = Date.now();
+  localStorage.setItem('Button_Data', JSON.stringify(ButtonData));
+  const endSave = Date.now();
+
+  return { data, start, end, startSave, endSave };
 };
